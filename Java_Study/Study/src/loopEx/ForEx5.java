2a9eb0cb8f467482b85eb94d9faf34b68bc2381d
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 18:22:41 +0900

    짝수 구구단

--- Java_Study/Study/src/loopEx/ForEx5.java
+++ Java_Study/Study/src/loopEx/ForEx5.java
@@ -5,15 +5,24 @@ public class ForEx5 {
 	public static void main(String[] args) {
 		// 짝수 단(2,4,8)만 출력
 
+//		for(int i=1;i<=9;i++) {
+//			if(i%2==0) {
+//				System.out.println(i + "단");
+//				for(int j=0; j<=9; j++) {
+//					System.out.println(i+"*"+j+"="+i*j);
+//				}
+//			}
+//		}
+		
+		//2단 2*2까지, 4단 4*4단까지 8단 8*까지
+		
 		for(int i=1;i<=9;i++) {
-			if(i%2==0) {
-				System.out.println(i + "단");
 				for(int j=0; j<=9; j++) {
+					if(i%2==0&&j<=i)
 					System.out.println(i+"*"+j+"="+i*j);
 				}
 			}
 		}
-
+		
 	}
 
-}
