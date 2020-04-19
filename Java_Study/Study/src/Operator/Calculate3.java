a4074b12b17a199b5a8c503881ddc1ca8e6e0487
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 16:59:23 +0900

    a>b>c 값 비교

--- Java_Study/Study/src/Operator/Calculate3.java
+++ Java_Study/Study/src/Operator/Calculate3.java
@@ -4,12 +4,15 @@ public class Calculate3 {
 
 	public static void main(String[] args) {
 		
+		//a>b>c 값 비교
+		
 		int a,b,c;
 		
 		a = (((25+5) + (36/4))-72) * 5;
 		b = (((25*5) + (36-4))-71) / 4;
 		c = (128/4)*2;
 		
+		//a>b>c 값 비교, 둘다 참 
 		if (a > b && b > c)
 		{
 			System.out.println("true");
