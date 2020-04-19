cf4ad2967c5dd1049922e999905e8213e49645a0
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 18:11:16 +0900

    switch문 숫자 비교

--- Java_Study/Study/src/ifEx/SwitchEx1.java
+++ Java_Study/Study/src/ifEx/SwitchEx1.java
@@ -1,40 +1,31 @@
 package ifEx;
 
+
+/*수정 : switch문 조건식 변경*/
+
 public class SwitchEx1 {
 
 	public static void main(String[] args) {
 
 		int num = 100;
-		int x = 0;
-		if (num < 0) {
+		
+		if(num<0) {
 			System.out.println("0미만");
-		} else {
-			if (num >= 300) {
-				x = 1;
-			} else if (num >= 200) {
-				x = 2;
-
-			} else if (num >= 100) {
-				x = 3;
-
-			} else if (num >= 0) {
-				x = 4;
-
-			}
 		}
-
-		switch (x) {
+		
+		switch(num/100) {
+		
+		case 0:
+			System.out.println("0이상 100미만");
+			break;
 		case 1:
-			System.out.println("300이상");
+			System.out.println("100이상 200미만");
 			break;
 		case 2:
 			System.out.println("200이상 300미만");
 			break;
-		case 3:
-			System.out.println("100이상 200미만");
-			break;
-		case 4:
-			System.out.println("0이상 100미만");
+		default:
+			System.out.println("300이상");
 			break;
 		}
 
