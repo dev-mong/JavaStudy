03b46e368916c832165fae1c6eaf2b0450070635
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 16:03:22 +0900

    문자열

---
+++ Java_Study/Study/src/stringex/StringEx2.java
@@ -0,0 +1,12 @@
+package stringex;
+
+public class StringEx2 {
+
+	public static void main(String[] args) {
+		
+		String str = "990929-1010123";
+		System.out.println(str.replace("-",""));
+
+	}
+
+}
