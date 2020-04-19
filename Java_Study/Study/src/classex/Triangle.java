18e5c25238e898a0666f3b991c49fd16bbd40513
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 16:03:10 +0900

    삼각형 넓이

--- Java_Study/Study/src/classex/Triangle.java
+++ Java_Study/Study/src/classex/Triangle.java
@@ -23,9 +23,9 @@ public class Triangle {
 	public static void main(String[] args) {
 		
 		Triangle t = new Triangle(2,5);
+		
 		System.out.println("삼각형의 밑변: "+t.width);
 		System.out.println("삼각형의 높이: "+t.height);
-		
 		System.out.println("삼각형의 넓이: "+t.areaTriangle());
 
 	}
