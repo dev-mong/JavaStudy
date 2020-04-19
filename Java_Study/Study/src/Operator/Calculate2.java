e59b5a524fefb49778fcaa847529780c88fd3030
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 16:57:02 +0900

    3+6,3+6+9,3+6+9+12 연산

--- Java_Study/Study/src/Operator/Calculate2.java
+++ Java_Study/Study/src/Operator/Calculate2.java
@@ -4,6 +4,7 @@ public class Calculate2 {
 
 	public static void main(String[] args) {
 
+		//3+6,3+6+9,3+6+9+12 연산 결과 
 		int num, sum;
 		sum = 0;
 		num = 3;
@@ -11,9 +12,15 @@ public class Calculate2 {
 		for (int x = 2; x < 5; x++) {
 
 			sum = num + x * 3;
-			System.out.println(sum);
 			num = sum;
 		}
+		System.out.println(sum);
+		
+		// 다른 풀이
+		int a=3+6;
+		int b=a+9;
+		int c=b+12;
+		System.out.println("연산 결과 : "+c);
 
 	}
 
