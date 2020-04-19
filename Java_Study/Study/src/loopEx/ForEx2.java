9ac6f89681d7394b6535c4586a524e9684770298
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 18:14:50 +0900

    구구단 5단 출력

--- Java_Study/Study/src/loopEx/ForEx2.java
+++ Java_Study/Study/src/loopEx/ForEx2.java
@@ -4,14 +4,22 @@ public class ForEx2 {
 
 	public static void main(String[] args) {
 
-		for (int i = 2; i <= 5; i++) {
-			System.out.println(i + "단");
-			for (int j = 1; j <= 9; j++) {
-				System.out.println(i + "X" + j + "=" + i * j);
-			}
-			System.out.println("----------------");
-		}
+		//구구단 5단까지 출력
+		
+//		for (int i = 2; i <= 5; i++) {
+//			System.out.println(i + "단");
+//			for (int j = 1; j <= 9; j++) {
+//				System.out.println(i + "X" + j + "=" + i * j);
+//			}
+//			System.out.println("----------------");
+//		}
 
+		// 구구단 5단 출력 
+		for(int i=1;i<=9;i++) {
+			System.out.println("5x"+i+"="+5*i);
+		}
+		
+		
 	}
 
 }
