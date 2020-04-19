388f8149b9609e58c4902b3ada739961cda567fb
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-19 17:59:34 +0900

    양수이면서 짝수

--- Java_Study/Study/src/ifEx/IfEx1.java
+++ Java_Study/Study/src/ifEx/IfEx1.java
@@ -4,6 +4,8 @@ public class IfEx1 {
 
 	public static void main(String[] args) {
 		int num = 120;
+		
+		//짝수 중 양수 인 값 , 둘다 참일 때 출력 
 		if(num>0 && (num % 2) == 0) {
 			System.out.println("양수이면서 짝수");
 		}
