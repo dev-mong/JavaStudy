0843ba34e3dcca3a906e2bce23bad16e2e85a6f0
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-20 10:20:35 +0900

    별 출력

--- Java_Study/Study/src/loopEx/WhileEx6.java
+++ Java_Study/Study/src/loopEx/WhileEx6.java
@@ -3,19 +3,7 @@ package loopEx;
 public class WhileEx6 {
 
 	public static void main(String[] args) {
-
-//			for(int i=0; i<=10; i++) {
-//
-//				for(int j=0; j<=i; j++) {
-//
-//					System.out.print("*");
-//				}
-//				
-//
-//			System.out.println();
-//
-//			}
-//			System.out.println("----------------");
+		//* 출력 
 
 		int i = 0;
 		while (true) {
