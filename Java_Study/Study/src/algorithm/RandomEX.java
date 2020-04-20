e7f071fdb4d296eece750d211cdd1519effbd6f2
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-20 10:23:46 +0900

    랜덤 함수

--- Java_Study/Study/src/algorithm/RandomEX.java
+++ Java_Study/Study/src/algorithm/RandomEX.java
@@ -4,7 +4,7 @@ public class RandomEX {
 
 	public static void main(String[] args) {
 		
-		int value = (int)(Math.random()*6+1);
+		int value = (int)(Math.random()*6+1); // 1부터 6사이 임의의 정수 
 		System.out.println("value:"+value);
 
 	}
