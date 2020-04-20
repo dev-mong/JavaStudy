e74aa130c5bcfecee098589b698ea40d26da3c88
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-20 10:05:59 +0900

    1부터 20까지의 정수 중 2 또는 3의 배수가 아닌 수의 총합

--- Java_Study/Study/src/loopEx/ForEx9.java
+++ Java_Study/Study/src/loopEx/ForEx9.java
@@ -5,10 +5,10 @@ public class ForEx9 {
 	public static void main(String[] args) {
 //		 1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합
 
-		int sum = 0;
+		int sum = 0; // 총 합 변수 초기화 
 		
 		for (int i = 1; i <= 20; i++) {
-			if(!(i%2==0 || i%3==0))
+			if(!(i%2==0 || i%3==0)) // if(i%2!=0||i%3!=0) 
 			{
 				sum += i;
 			}
