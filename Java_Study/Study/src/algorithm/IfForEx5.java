3b847046dadfdbf9c62aa5655584809537c2095e
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-20 10:34:54 +0900

    문자열 숫자 변환

--- Java_Study/Study/src/algorithm/IfForEx5.java
+++ Java_Study/Study/src/algorithm/IfForEx5.java
@@ -1,17 +1,17 @@
 package algorithm;
 
 
-public class IfForEx6 {
+public class IfForEx5 {
 
 	public static void main(String[] args) {
 //		문자열이"12345"라면, ‘1+2+3+4+5’의 결과인 15
 
 		String str = "12345";
 		int sum = 0;
-		int num;
+		int num; //문자열 숫자로 변환 후 저장 할 변수 
 		
-		for(int i=0;i<str.length();i++) {
-			num= str.charAt(i); // char 문자의 숫자 아스키 코드 값
+		for(int i=0;i<str.length();i++) { //문자열 길이만큼 반복 
+			num= str.charAt(i); // charAt(인덱스) 
 			sum += num - 48;
 		}System.out.println(sum);
 		
