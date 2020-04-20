76b40f35361a389cf525b0f720474cc0004eadd8
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-20 10:11:41 +0900

    1+(1+2)+(1+2+3)+...+(1+2+3+...+10)

--- Java_Study/Study/src/loopEx/ForEx10.java
+++ Java_Study/Study/src/loopEx/ForEx10.java
@@ -3,13 +3,24 @@ package loopEx;
 public class ForEx10 {
 
 	public static void main(String[] args) {
-//		 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)
+//		 1+(1+2)+(1+2+3)+...+(1+2+3+...+10)
 
-		int sum = 0;
-		int total = 0;
-		for (int i = 1; i <= 10; i++) {
-			sum += i;
-//			System.out.println(sum);
+// 		int sum = 0;
+// 		int total = 0;
+		
+// 		for (int i = 1; i <= 10; i++) {
+// 			sum += i;
+// 			total += sum;
+// 		}
+// 		System.out.println("1+(1+2)+(1+2+3)+ .. +(1+2+3+...+10) = "+total);
+		
+		int total=0;
+		
+		for(int i=1;i<=10;i++){
+			int sum=0;
+			for(int j=1;j<=i;j++){
+				sum+=j;
+			}
 			total += sum;
 		}
 		System.out.println("1+(1+2)+(1+2+3)+ .. +(1+2+3+...+10) = "+total);
