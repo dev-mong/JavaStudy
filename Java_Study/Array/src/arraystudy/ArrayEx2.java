c36d4290b1d1d5997c9cc05c186ab7604915ed50
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-22 10:31:07 +0900

    행과 열의 값 증가

---
+++ Java_Study/Array/src/arraystudy/ArrayEx2.java
@@ -0,0 +1,58 @@
+package arraystudy;
+
+import java.util.Scanner;
+
+public class ArrayEx2 {
+
+	int[][] arr = new int[3][3];
+	
+	int[][] data(int[][] arr, int add) {
+		arr[0][0] = 0;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j] += add;
+				add++;
+				this.arr = arr;
+				System.out.print(arr[i][j]);
+			} System.out.println();
+		}
+		return arr;
+	}
+	
+	void change() {
+		
+		int change[][] = new int[3][3];
+		
+//		for(int i=0;i<change.length;i++) {
+//			for(int j=0;j<change[i].length;j++) {
+//				change [i][j] = this.arr[1-i][j];
+//				System.out.print(change[i][j]+" ");
+//			} System.out.println();
+		
+			for(int i=0;i<arr[0].length;i++) {			
+				change[0][i] = this.arr[2][i];
+				System.out.print(change[0][i]+" ");
+			}System.out.println();
+			
+			for(int i=1;i<arr.length;i++) {
+				for(int j=0;j<arr[i].length;j++) {
+				change[i][j] = this.arr[i-1][j];
+				System.out.print(change[i][j]+" ");
+				}System.out.println();
+			}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[3][3];
+
+		ArrayEx2 array = new ArrayEx2();
+		
+		array.data(arr, 1);
+		System.out.println("-------------");
+		array.change();
+		
+	}
+}
