163d7cfcd29c2880450dfafcc59459e2e60a726a
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-24 15:02:38 +0900

    생성자 초기화 예제

--- Java_Study/Inheritance/src/ex/car.java
+++ Java_Study/Inheritance/src/ex/car.java
@@ -2,9 +2,42 @@ package ex;
 
 public class car {
 
+	int gasoilnegauge;
+	
+	
+	car(int gasoil){
+		this.gasoilnegauge=gasoil;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 }
+
+class HybridCar extends car{
+	
+	int electroniGauge;
+	
+	HybridCar(int gasoil,int elcetron) {
+		super(gasoil); //car 초기화
+		this.electroniGauge = elcetron;
+	}
+}
+
+
+class HybridWaterCar extends HybridCar{
+
+	int hybridWater;
+	
+	HybridWaterCar(int gasoil, int elcetron,int water) {
+		super(gasoil, elcetron);
+		this.hybridWater = water;
+		this.electroniGauge = water;
+	}
+	
+}
+
+
+
