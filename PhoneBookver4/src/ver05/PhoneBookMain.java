834c94f9be6624b13a89eac4b40d2e21598d96ce
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-04-29 00:58:33 +0900

    PhoneBook 추상클래스 인터페이스

---
+++ PhoneBookver4/src/ver05/PhoneBookMain.java
@@ -0,0 +1,15 @@
+package ver05;
+
+
+
+public class PhoneBookMain {
+
+	
+	public static void main(String[] args) {
+		
+
+		PhoneBookMenu menuChoose= new PhoneBookMenu(); 
+		menuChoose.menu();
+	}
+
+}
