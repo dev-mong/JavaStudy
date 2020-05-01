848168ef5d4ac89250c0d910317ded6191d68425
a4-01 <qkrwlsaud0118@gmail.com>
2020-05-01 14:55:41 +0900

    PhoneBook ver0.6 리스트

---
+++ Java_Study/PhoneBookver4/src/ver06/MenuInterface.java
@@ -0,0 +1,17 @@
+package ver06;
+
+public interface MenuInterface {
+	
+	int INSERT=1;
+	int ALLSHOW=2;
+	int SELET=3;
+	int UPDATE=4;
+	int DELETE=5;
+	int EXIT=6;
+	
+	int FRIEND=1;
+	int UNIV=2;
+	int COMPANY=3;
+	int CLUB=4;
+	
+}
