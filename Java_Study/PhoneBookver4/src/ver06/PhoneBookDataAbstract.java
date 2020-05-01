5f609556b41d5344680c919206ed75a101be334e
a4-01 <qkrwlsaud0118@gmail.com>
2020-05-01 14:55:23 +0900

    PhoneBook ver0.6 리스트

---
+++ Java_Study/PhoneBookver4/src/ver06/PhoneBookDataAbstract.java
@@ -0,0 +1,27 @@
+package ver06;
+
+public abstract class PhoneBookDataAbstract implements PhoneBookInterface{
+
+	
+	String name;
+	String phoneNumber;
+	
+	PhoneBookDataAbstract(String name,String phoneNumber){
+		this.name=name;
+		this.phoneNumber=phoneNumber;
+	}
+
+	
+	public void showData() {
+		if(this.name==null&&this.phoneNumber==null) {
+			System.out.println("데이터가 존재하지 않습니다.");
+		} else {
+			System.out.println("이름: "+this.name);
+			System.out.println("전화번호: "+this.phoneNumber);
+		}
+	}
+	boolean name(String nameInput) {
+		return this.name.equals(nameInput);
+	}
+	
+}
