f8b935232870ec74ae4ebee12f54bea33e815119
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-07 14:08:43 +0900

    BixBox Movie ver0.4

---
+++ Java_Study/BitBox ver2/src/Movie/MenuIf.java
@@ -0,0 +1,18 @@
+package Movie;
+
+public interface MenuIf  {
+	
+	//메뉴 선택 
+	int SHOWALL=1;
+	int ADD=2;
+	int EDIT=3;
+	int DEL=4;
+	int MENUHOME=5;
+	
+	//수정 메뉴 선택 
+	int EDITMENU1=1;
+	int EDITMENU2=2;
+	int EDITMENU3=3;
+	int EDITMENU4=4;
+	int EDITMENU5=5;
+}
