1b04d80c980a725a29e4af6fa70313f9e9a61994
a4-01 <qkrwlsaud0118@gmail.com>
2020-05-13 10:27:06 +0900

    PhoneBook ver0.5

---
+++ Java_Study/PhoneBookProject/src/ver05/PhoneBookClubInfor.java
@@ -0,0 +1,18 @@
+package ver05;
+
+//동호회 전화번호부
+public class PhoneBookClubInfor extends PhoneBookData{
+
+	String clubName;
+	
+	PhoneBookClubInfor(String name,String phoneNumber,String clubName) {
+		super(name,phoneNumber);
+		this.clubName=clubName;
+	}
+	
+	void showData() {
+		super.showData();
+		System.out.println("동호회 이름: "+this.clubName);
+	}
+	
+}
---
+++ Java_Study/PhoneBookProject/src/ver05/PhoneUnivlnfor.java
@@ -0,0 +1,33 @@
+package ver05;
+
+
+//대학교 전화번호부
+public class PhoneUnivlnfor extends PhoneBookData{
+
+	
+	String name; //이름
+	String phoneNumber; // 전화번호
+	String address; //주소 , 필수 사항 아님
+	String email; // 이메일, 필수 사항 아님
+	String major; // 전공
+	String year; // 학년
+	
+	
+	PhoneUnivlnfor(String name,String phoneNumber,String address,String email,String major,String year){
+		super(name,phoneNumber);
+		this.address=address;
+		this.email=email;
+		this.major=major;
+		this.year=year;	
+	}
+
+	void showData() {
+		super.showData();
+		System.out.println("주소: "+this.address);
+		System.out.println("이메일: "+this.email);
+		System.out.println("전공: "+this.major);
+		System.out.println("학년 : "+this.year);
+	}
+	
+	
+}
